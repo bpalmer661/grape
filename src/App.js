d39146873216e01199f1a07944dd263ddd7ce220
benpalmer661 <BenPalmer@Bens-MacBook-Pro-2.local>
2020-05-17 20:13:49 +0800

    lesson 3

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter } from 'react-router-dom' 
+
 function App() {
   return (
+
+<BrowserRouter> 
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     GRAPE
     </div>
+
+    </BrowserRouter>
+   
+you need to edit the end of part 2 video to go with part 3 
+
+
+
+
   );
 }
 
