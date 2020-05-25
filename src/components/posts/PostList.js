22d0f030f403163f5c587ec5f4ce57d3f63510c8
benpalmer661 <BenPalmer@Bens-MBP-2.lan>
2020-05-25 19:01:25 +0800

    lesson-6

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import PostSummary from './PostSummary'
 
 const PostList = () => {
 
@@ -7,21 +7,21 @@ const PostList = () => {
 
         <div className="project-list section">
           
-    
-            <div className="card z-depth-0 project-summary">
-            <div className="card-content grey-text text-darken-2">
-   <span className="card-title z-depth-0 project-summary">
-       Post Title
-   </span>
-   <p>Posted By Ben</p>
-   <p className="grey-text">20th May 2020</p>
-</div>
-            </div>
-
-
-
+          <PostSummary />
+         
+          <PostSummary />
+         
+          <PostSummary />
+         
+          <PostSummary />
+         
+          <PostSummary />
+         
+          <PostSummary />
+         
+          <PostSummary />
+         
           
-
           
 
           
---
+++ src/components/posts/PostSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const PostSummary = () => {
+return (
+
+    <div className="card z-depth-0 project-summary">
+    <div className="card-content grey-text text-darken-2">
+<span className="card-title z-depth-0 project-summary">
+Post Title
+</span>
+<p>Posted By Ben</p>
+<p className="grey-text">20th May 2020</p>
+</div>
+    </div>
+
+
+)
+}
+
+
+export default PostSummary
