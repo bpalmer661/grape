ad660889eb7562e9b225a43311b052084f188a21
benpalmer661 <BenPalmer@Bens-MBP-2.lan>
2020-06-13 08:53:16 +0800

    lesson-22

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -5,30 +5,46 @@ import SignedOutLinks from './SignedOutLinks';
 import SignedInLinks from './SignedInLinks';
 import { connect } from 'react-redux'
 
+////////////////////////////////
+const Navbar = (props) => {
 
-const Navbar = () => {
+
+const { auth } = props;
+
+console.log(auth);
+
+const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
+
+////////////////////////////////
+
+
+
+
     return(
         <nav className="nav-wrapper grey darken-3">
 <div className="container">
    <Link to='/' className="brand-logo">Grape</Link>
-   <SignedInLinks />
-   <SignedOutLinks />
-   
+
+   {/* ///////////////// */}
+   {auth.isLoaded && 
+  links
+}
+   {/* ///////////////// */}
 
 </div>
         </nav>
     )
 }
 
-/////////////////////////////////////////////////////
+
+////////////////////////////////
 const mapStateToProps = (state) => {
     console.log(state)
     return{
-
+        auth: state.firebase.auth
     }
 }
-
+////////////////////////////////
 
 
 export default connect(mapStateToProps)(Navbar)
-///////////////////////////////////////////////////
